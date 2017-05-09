36412e0f42217c603c18462b6d7262bf7bad7269
Alejandro <A01370909@itesm.mx>
2017-05-09 10:45:15 -0500

    Initial commit

---
+++ app/src/main/java/mx/itesm/acmjscb/metodosnumericos/MenuPrincipal.java
@@ -0,0 +1,13 @@
+package mx.itesm.acmjscb.metodosnumericos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
